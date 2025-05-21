235f6d9b651fa789e2c7dbc8cbba616dff65596f
square73 <paparoupa5@proton.me>
2025-05-22 01:45:06 +0300

    Update loss_models.py

--- loss_models.py
+++ loss_models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from utils.utils import get_device
 
@@ -8,8 +9,8 @@ class VanillaMultiLoss(nn.Module):
         super().__init__()
         self.loss_term_scaling = nn.Parameter(torch.ones(n_losses, device=get_device()))
 
-    def forward(self, losses: list) -> torch.tensor:
-        total_loss = 0
+    def forward(self, losses: list) -> torch.Tensor:
+        total_loss = torch.zeros(1, device=losses[0].device, dtype=losses[0].dtype)
         for i, loss in enumerate(losses):
             scaling = F.softplus(self.loss_term_scaling[i])  # ensure > 0
 
@@ -25,7 +26,7 @@ class MultiNoiseLoss(nn.Module):
         super(MultiNoiseLoss, self).__init__()
         self.raw_noise_params = nn.Parameter(torch.randn(n_losses, device=get_device())) # learnable normal noise
     
-    def forward(self, losses: list) -> torch.tensor:
+    def forward(self, losses: list) -> torch.Tensor:
         """
         Computes the total loss as a function of a list of classification losses.
         TODO: Handle regressions losses, which require a factor of 2 (see arxiv.org/abs/1705.07115 page 4)
@@ -33,7 +34,7 @@ class MultiNoiseLoss(nn.Module):
         Each loss coeff is of the form: :math:`\frac{1}{\sqrt{\eta_i}} \cdot \ell_i + \log(\eta_i)`
         Total loss: :math:`\ell = \sum_{i=1}^{k} \left\[ \frac{1}{\sqrt{\eta_i}} \cdot \ell_i + \log(\eta_i) \right\]`
         """
-        total_loss = 0
+        total_loss = torch.zeros(1, device=losses[0].device, dtype=losses[0].dtype)
         for i, loss in enumerate(losses):
             noise_i = F.softplus(self.raw_noise_params[i]) + 1e-6  # ensure > 0
             total_loss += (1 / noise_i ** 2) * loss + torch.log(noise_i)
